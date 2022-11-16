afc0fefa901079a5c38e1863b6e6d22e691ef59c
ponyjackal <pony1125@outlook.com>
2022-11-15 19:29:56 -0800

    feat: add Makefile

--- src/NFT.sol
+++ src/NFT.sol
@@ -5,9 +5,18 @@ import { ERC721 } from "solmate/tokens/ERC721.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+error MintPriceNotPaid();
+error MaxSupply();
+error NonExistentTokenURI();
+error WithdrawTransfer();
+
 contract NFT is ERC721, Ownable {
     using Strings for uint256;
+
+    string public baseURI;
     uint256 public currentTokenId;
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.08 ether;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
